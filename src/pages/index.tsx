014c0d4ce6772a25dead4c760d77f45c386c03fa
Marcus Martins <marcusmartins38@gmail.com>
2024-05-24 20:16:27 -0400

    chore: use gestatic props, prefetch false

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,63 +10,63 @@ import Stripe from 'stripe';
 import Link from 'next/link';
 
 interface HomeProps {
-  products: {
-    id: string;
-    name: string;
-    imageUrl: string;
-    price: number;
-  }[];
+    products: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: string;
+    }[];
 }
 
 export default function Home({ products }: HomeProps) {
-  const [sliderRef] = useKeenSlider({
-    slides: {
-      perView: 3,
-      spacing: 48,
-    },
-  });
+    const [sliderRef] = useKeenSlider({
+        slides: {
+            perView: 3,
+            spacing: 48,
+        },
+    });
 
-  return (
-    <HomeContainer ref={sliderRef}>
-      {products.map((product) => (
-        <Link href={`/products/${product.id}`} key={product.id}>
-          <Product className="keen-slider__slide">
-            <Image src={product.imageUrl} width={400} height={500} alt="first" />
+    return (
+        <HomeContainer ref={sliderRef}>
+            {products.map((product) => (
+                <Link href={`/products/${product.id}`} key={product.id} prefetch={false}>
+                    <Product className="keen-slider__slide">
+                        <Image src={product.imageUrl} width={400} height={500} alt="first" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        </Link>
-      ))}
-    </HomeContainer>
-  );
+                        <footer>
+                            <strong>{product.name}</strong>
+                            <span>{product.price}</span>
+                        </footer>
+                    </Product>
+                </Link>
+            ))}
+        </HomeContainer>
+    );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await stripe.products.list({
-    expand: ['data.default_price'],
-  });
+    const response = await stripe.products.list({
+        expand: ['data.default_price'],
+    });
 
-  const products = response.data.map((product) => {
-    const price = product.default_price as Stripe.Price;
+    const products = response.data.map((product) => {
+        const price = product.default_price as Stripe.Price;
+
+        return {
+            id: product.id,
+            name: product.name,
+            imageUrl: product.images[0],
+            price: new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+            }).format(price.unit_amount! / 100),
+        };
+    });
 
     return {
-      id: product.id,
-      name: product.name,
-      imageUrl: product.images[0],
-      price: new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      }).format(price.unit_amount / 100),
+        props: {
+            products,
+        },
+        revalidate: 60 * 60 * 2, // 2 hours
     };
-  });
-
-  return {
-    props: {
-      products,
-    },
-    revalidate: 60 * 60 * 2, // 2 hours
-  };
 };
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,21 +1,66 @@
+import { stripe } from '@/lib/stripe';
 import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Stripe from 'stripe';
 
-export default function Product() {
-  const { query } = useRouter();
-
-  return (
-    <ProductContainer>
-      <ImageContainer></ImageContainer>
-
-      <ProductDetails>
-        <h1></h1>
-        <span></span>
-
-        <p></p>
-
-        <button>Buy Now</button>
-      </ProductDetails>
-    </ProductContainer>
-  );
+interface ProductProps {
+    product: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: string;
+        description: string;
+    };
 }
+
+export default function Product({ product }: ProductProps) {
+    return (
+        <ProductContainer>
+            <ImageContainer>
+                <Image src={product.imageUrl} width={520} height={480} alt="T-shirt" />
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
+
+                <p>{product.description}</p>
+
+                <button>Buy Now</button>
+            </ProductDetails>
+        </ProductContainer>
+    );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: 'blocking',
+    };
+};
+
+export const getServerSideProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    const productId = params!.id;
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ['default_price'],
+    });
+    const price = product.default_price as Stripe.Price;
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                price: new Intl.NumberFormat('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                }).format(price.unit_amount! / 100),
+            },
+        },
+        revalidate: 60 * 60 * 1, // 1 hour
+    };
+};
